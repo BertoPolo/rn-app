9848d64a28b6b109fc6f802d46d18652fb287d7e
Alberto <bertopolo91@gmail.com>
2025-01-28 19:58:41 +0100

    .

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { StatusBar, Text, View } from "react-native"
+
+// This is "/"
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Gimme a Siuuuuuuuu</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
